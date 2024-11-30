a02d4f97593ad21099282705798060d1da84490a
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:26:25 +0530

    initial commit

---
+++ src/test/java/com/crowdseats/cs_api_gateway/CsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdseats.cs_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
